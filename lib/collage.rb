16e3b00e29563d44bbd2b071ff3a792fd28801ab
Pankaj Doharey <pankajdoharey@live.com>
2016-05-30 02:09:02 +0530

    Updates collage build search method to compensate and add more words from dictioanry if provided with less search keywords governed by a min word count in config yaml

--- lib/collage.rb
+++ lib/collage.rb
@@ -2,8 +2,18 @@ class Collage
   attr_reader :search_words
 
   def initialize(words)
-    @search_words = words
+    @search_words = build_search words
   end
 
+  private
 
+  def build_search words
+    if words.count < MIN_WORD_COUNT
+      (MIN_WORD_COUNT - words.count).times do
+        words << @@dictionary.sample
+      end
+    end
+
+    words
+  end
 end
