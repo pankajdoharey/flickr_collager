ce63003f6e5f71c53aa088b0e7ab83a5f687af33
Pankaj Doharey <pankajdoharey@live.com>
2016-05-30 02:07:10 +0530

    Updates and adds case for dynamically handling config options for values that are String and others

--- lib/app_config/options.rb
+++ lib/app_config/options.rb
@@ -1,5 +1,12 @@
 module ::AppConfig::Options
   def self.set_options config
-    config["options"].each {|k, v| self.class_eval("#{k}=#{v}")}
+    config["options"].each do |key, val|
+      case val
+      when String
+        self.class_eval("#{key}='#{val}'")
+      else
+        self.class_eval("#{key}=#{val}")
+      end
+    end
   end
 end
