5540523d503c4c1bd333b770756f225b643fe27b
Pankaj Doharey <pankajdoharey@live.com>
2016-05-30 02:17:25 +0530

    Updates options module to set constants using const_set instead of clas_eval

--- lib/app_config/options.rb
+++ lib/app_config/options.rb
@@ -1,12 +1,7 @@
 module ::AppConfig::Options
   def self.set_options config
     config["options"].each do |key, val|
-      case val
-      when String
-        self.class_eval("#{key}='#{val}'")
-      else
-        self.class_eval("#{key}=#{val}")
-      end
+      self.const_set(key, val)
     end
   end
 end
