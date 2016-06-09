40979813f02d09e10e96904cf80faffb8acc59ff
Pankaj Doharey <pankajdoharey@live.com>
2016-06-09 20:06:54 +0530

    Updates code and removes unnecessary __FILE_+ contant detail.

--- lib/post_install.rb
+++ lib/post_install.rb
@@ -2,7 +2,7 @@ module AppConfig::PostInstall
   def self.message
     unless File.directory?(File.expand_path('~/.collage', __FILE__))
       FileUtils.mkdir(File.expand_path('~/.collage/'))
-      FileUtils.cp('config.yaml', File.expand_path('~/.collage/config.yaml', __FILE__))
+      FileUtils.cp('config.yaml', File.expand_path('~/.collage/config.yaml'))
       puts <<-MESSAGE
       A basic Yaml Configuration file has been copied to ~/.collage/config.yaml
       you modify that to change some setting.
